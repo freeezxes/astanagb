9fd1900901c28aaf9433828706fc6434baff2a6a
ayuldasho <alizhan.yuldashov@gmail.com>
2025-08-04 09:21:00 +0500

    final

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")  # ищет в папке templates/
+    return render_template("index.html")
+
+# --- новые маршруты ---
+@app.route("/offer")
+def offer():
+    return render_template("offer.html")
+
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+# ----------------------
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
-
